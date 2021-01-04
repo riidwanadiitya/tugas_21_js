85a00e8f467c8328ab8d310c2362a1ce49dc4b7f
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 09:38:51 +0700

    TUGAS 21 JS

---
+++ tugas21.js
@@ -0,0 +1,7 @@
+function berubah() {
+    var satu = "Saya belajar masak nasi goreng";
+    console.log(satu);
+    var ubah = satu.replace("masak", "makan");
+    return ubah;
+}
+console.log(berubah());
